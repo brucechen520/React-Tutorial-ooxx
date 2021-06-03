10e2d0b559f6ea1751c7ec283e91b4d7fa67361b
Bruce.Chen <gibe329apt978@gmail.com>
2021-06-03 16:41:31 +0800

    改寫 Board，使用兩個 loop 建立方格而不是寫死它。

--- src/index.js
+++ src/index.js
@@ -23,21 +23,15 @@ function Square(props) {
     render() {
       return (
         <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+          {
+            [0, 3, 6].map(index => {
+                return <div className="board-row">
+                    {this.renderSquare(index)}
+                    {this.renderSquare(index+1)}
+                    {this.renderSquare(index+2)}
+                </div>
+            })
+          }
         </div>
       );
     }
